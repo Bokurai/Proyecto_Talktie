3d0f76a0c4d30938c289d52881834c91cccaf74a
Bokurai <mrnere88@gmail.com>
2024-03-04 19:37:23 +0100

    Cambiando lógica login

--- app/src/main/java/com/example/proyecto_talktie/Login.java
+++ app/src/main/java/com/example/proyecto_talktie/Login.java
@@ -4,8 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.proyecto_talktie.databinding.FragmentLoginBinding;
@@ -16,18 +16,33 @@ public class Login extends Fragment {
 
     FragmentLoginBinding binding;
 
-    private FirebaseAuth mAuth;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        mAuth = FirebaseAuth.getInstance();
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
 
+        String mail = binding.etEmailOne.getText().toString();
+        String password = binding.etPasss.getText().toString();
+
+        binding.btnLogIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                  mAuth.signInWithEmailAndPassword(mail, password);
+            }
+        });
     }
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return (binding = FragmentLoginBinding.inflate(inflater, container, false)).getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle
+            savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
     }
 }
