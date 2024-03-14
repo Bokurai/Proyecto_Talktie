0b964aa78bc3228dcb32e63d2167c9979b13d5b7
Bokurai <mrnere88@gmail.com>
2024-03-14 18:36:52 +0100

    Cambiando lógica login

--- app/src/main/java/com/example/proyecto_talktie/SignIn1.java
+++ app/src/main/java/com/example/proyecto_talktie/SignIn1.java
@@ -1,13 +1,23 @@
 package com.example.proyecto_talktie;
 
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.proyecto_talktie.databinding.FragmentLoginBinding;
+import com.example.proyecto_talktie.databinding.FragmentSignIn1Binding;
+import com.google.firebase.auth.FirebaseAuth;
+
 
 public class SignIn1 extends Fragment {
+
+    FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    FragmentSignIn1Binding binding;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -16,9 +26,17 @@ public class SignIn1 extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_in1, container, false);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return (binding = FragmentSignIn1Binding.inflate(inflater, container, false)).getRoot();
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle
+            savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.etEmail.setEnabled(false);
+        binding.etEmail.setText(mAuth.getCurrentUser().getEmail());
     }
 }
