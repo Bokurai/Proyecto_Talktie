753701d8a5970edf921529df3f472b3ce4cd9b90
Bokurai <mrnere88@gmail.com>
2024-03-21 20:00:18 +0100

    modificando valores y viewmodel_registro student

--- app/src/main/java/com/example/proyecto_talktie/SignIn6.java
+++ app/src/main/java/com/example/proyecto_talktie/SignIn6.java
@@ -2,18 +2,87 @@ package com.example.proyecto_talktie;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.Timestamp;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 
 public class SignIn6 extends Fragment {
+
+    StudentRegisterViewModel registerViewModel;
+    NavController navController;
+
+    EditText nameET, phoneET, emailET, dateET;
+
+    AppCompatButton nextButton;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        registerViewModel = new ViewModelProvider(requireActivity()).get(StudentRegisterViewModel.class);
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_sign_in6, container, false);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        nameET = view.findViewById(R.id.etNameSignOne);
+        phoneET = view.findViewById(R.id.etPhoneNumber);
+        emailET = view.findViewById(R.id.etEmailOne);
+        dateET = view.findViewById(R.id.birth_date);
+        nextButton = view.findViewById(R.id.btnContinueOne);
+        navController = Navigation.findNavController(view);
+
+        nameET.setEnabled(false);
+        emailET.setEnabled(false);
+        phoneET.setEnabled(false);
+        nameET.setText(registerViewModel.getName().toString());
+        emailET.setText(registerViewModel.getEmail().toString());
+        phoneET.setText(registerViewModel.getPhoneNumber().toString());
+
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Date date = EditTextToDate(dateET);
+                Timestamp timestamp = new Timestamp(date);
+                registerViewModel.setBirth_date(timestamp.toDate());
+                navController.navigate(R.id.signIn7);
+            }
+        });
+    }
+
+    private Date EditTextToDate(EditText dateEditText) {
+        String dateString = dateEditText.getText().toString();
+
+        try {
+            SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+            Date date = sdf.parse(dateString);
+            return date;
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
 }
--- app/src/main/java/com/example/proyecto_talktie/StudentRegisterViewModel.java
+++ app/src/main/java/com/example/proyecto_talktie/StudentRegisterViewModel.java
@@ -7,6 +7,8 @@ import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 
+import java.util.Date;
+
 public class StudentRegisterViewModel extends AndroidViewModel {
     public StudentRegisterViewModel(@NonNull Application application) {
         super(application);
@@ -22,8 +24,22 @@ public class StudentRegisterViewModel extends AndroidViewModel {
     private MutableLiveData<String> phoneNumber = new MutableLiveData<>();
     private MutableLiveData<Integer> profileImage = new MutableLiveData<>();
     private MutableLiveData<String> website = new MutableLiveData<>();
+    private MutableLiveData<Date> birth_date = new MutableLiveData<>();
+    private MutableLiveData<String> gender = new MutableLiveData<>();
+    private MutableLiveData<Boolean> currently_working = new MutableLiveData<>();
+    private MutableLiveData<String> studentId = new MutableLiveData<>();
+    private MutableLiveData<String> center = new MutableLiveData<>();
+    private MutableLiveData<String> company = new MutableLiveData<>();
+    private MutableLiveData<Date> start_date_job = new MutableLiveData<>();
+    private MutableLiveData<Date> end_date_job = new MutableLiveData<>();
+    private MutableLiveData<Date> start_date_formation = new MutableLiveData<>();
+    private MutableLiveData<Date> end_date_formation = new MutableLiveData<>();
+    private MutableLiveData<String> location = new MutableLiveData<>();
+    private MutableLiveData<String> job_categories = new MutableLiveData<>();
+    private MutableLiveData<String> degree = new MutableLiveData<>();
 
-    // Métodos para actualizar los datos del usuario
+
+
     public void setId(String id) {
         this.id.setValue(id);
     }
@@ -102,5 +118,109 @@ public class StudentRegisterViewModel extends AndroidViewModel {
 
     public LiveData<String> getWebsite() {
         return website;
+    }
+
+    public LiveData<Date> getBirth_date() {
+        return birth_date;
+    }
+
+    public void setBirth_date(Date birth_date) {
+        this.birth_date.setValue(birth_date);
+    }
+
+    public LiveData<String> getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender.setValue(gender);
+    }
+
+    public LiveData<Boolean> getCurrently_working() {
+        return currently_working;
+    }
+
+    public void setCurrently_working(Boolean currently_working) {
+        this.currently_working.setValue(currently_working);
+    }
+
+    public LiveData<String> getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId.setValue(studentId);
+    }
+
+    public LiveData<String> getCenter() {
+        return center;
+    }
+
+    public void setCenter(String center) {
+        this.center.setValue(center);
+    }
+
+    public LiveData<String> getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company.setValue(company);
+    }
+
+    public LiveData<Date> getStart_date_job() {
+        return start_date_job;
+    }
+
+    public void setStart_date_job(Date start_date_job) {
+        this.start_date_job.setValue(start_date_job);
+    }
+
+    public LiveData<Date> getEnd_date_job() {
+        return end_date_job;
+    }
+
+    public void setEnd_date_job(Date end_date_job) {
+        this.end_date_job.setValue(end_date_job);
+    }
+
+    public LiveData<Date> getStart_date_formation() {
+        return start_date_formation;
+    }
+
+    public void setStart_date_formation(Date start_date_formation) {
+        this.start_date_formation.setValue(start_date_formation);
+    }
+
+    public LiveData<Date> getEnd_date_formation() {
+        return end_date_formation;
+    }
+
+    public void setEnd_date_formation(Date end_date_formation) {
+        this.end_date_formation.setValue(end_date_formation);
+    }
+
+    public LiveData<String> getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location.setValue(location);
+    }
+
+    public LiveData<String> getJob_categories() {
+        return job_categories;
+    }
+
+    public void setJob_categories(String job_categories) {
+        this.job_categories.setValue(job_categories);
+    }
+
+    public LiveData<String> getDegree() {
+        return degree;
+    }
+
+    public void setDegree(String degree) {
+        this.degree.setValue(degree);
     }
 }
