063ca406b7e8e978547175a22de85fd0c1e1d4c4
mina-45b <bmonroy197@gmail.com>
2024-03-21 19:04:41 +0100

    Ofertas viewholder, viewmodel y objeto

---
+++ app/src/main/java/com/example/proyecto_talktie/OfferObject.java
@@ -0,0 +1,82 @@
+package com.example.proyecto_talktie;
+
+import java.util.Date;
+import java.util.List;
+
+public class OfferObject {
+
+    String offerId;
+    String name;
+    String companyId;
+    List<String> tags;
+    Date date;
+    //De aquí se puede extraer el número de aplicantes a la oferta
+    List<String> applicantsId;
+
+    //Si el id del usuario esta en la lista de aplicante se vuelve true;
+    boolean isAppliend;
+
+    public OfferObject() {
+    }
+
+    public OfferObject(String name, String companyId) {
+        this.name = name;
+        this.companyId = companyId;
+    }
+
+    public String getOfferId() {
+        return offerId;
+    }
+
+    public void setOfferId(String offerId) {
+        this.offerId = offerId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCompanyId() {
+        return companyId;
+    }
+
+    public void setCompanyId(String companyId) {
+        this.companyId = companyId;
+    }
+
+    public List<String> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<String> tags) {
+        this.tags = tags;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public List<String> getApplicantsId() {
+        return applicantsId;
+    }
+
+    public void setApplicantsId(List<String> applicantsId) {
+        this.applicantsId = applicantsId;
+    }
+
+    public boolean isAppliend() {
+        return isAppliend;
+    }
+
+    public void setAppliend(boolean appliend) {
+        isAppliend = appliend;
+    }
+}
--- app/src/main/java/com/example/proyecto_talktie/Recommendation.java
+++ app/src/main/java/com/example/proyecto_talktie/Recommendation.java
@@ -3,17 +3,47 @@ package com.example.proyecto_talktie;
 public class Recommendation {
     private String idTeacher;
     private String recommendationText;
+    private String teacherName;
+    private String teacherImage;
 
-    public Recommendation(String idTeacher, String recommendationText) {
+    public Recommendation() {
+    }
+
+    public Recommendation(String idTeacher, String recommendationText, String teacherName) {
         this.idTeacher = idTeacher;
         this.recommendationText = recommendationText;
+        this.teacherName = teacherName;
     }
 
     public String getIdTeacher() {
         return idTeacher;
     }
 
+    public void setIdTeacher(String idTeacher) {
+        this.idTeacher = idTeacher;
+    }
+
     public String getRecommendationText() {
         return recommendationText;
+    }
+
+    public void setRecommendationText(String recommendationText) {
+        this.recommendationText = recommendationText;
+    }
+
+    public String getTeacherName() {
+        return teacherName;
+    }
+
+    public void setTeacherName(String teacherName) {
+        this.teacherName = teacherName;
+    }
+
+    public String getTeacherImage() {
+        return teacherImage;
+    }
+
+    public void setTeacherImage(String teacherImage) {
+        this.teacherImage = teacherImage;
     }
 }
