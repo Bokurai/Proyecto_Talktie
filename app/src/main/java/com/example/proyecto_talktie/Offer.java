b084d32cb6f9758e1f5039280e099da5c5bbf401
mina-45b <bmonroy197@gmail.com>
2024-03-22 16:30:08 +0100

    estilos botones

--- app/src/main/java/com/example/proyecto_talktie/Offer.java
+++ app/src/main/java/com/example/proyecto_talktie/Offer.java
@@ -71,19 +71,25 @@ public class Offer extends Fragment {
 
             OfferObject offerObject = offerObjectList.get(position);
 
-            holder.offerName.setText(offerObject.getName());
+            holder.name.setText(offerObject.getName());
             holder.companyName.setText(offerObject.getCompanyId());
 
-            if (offerObject.getTags().get(0) != null) {
+            if (offerObject.getTags().size() > 0) {
                 holder.tag1.setText(offerObject.getTags().get(0));
+            } else {
+                holder.tag1.setVisibility(View.GONE); // Si no hay etiqueta, oculta la vista
             }
 
-            if (offerObject.getTags().get(1) != null) {
-                holder.tag1.setText(offerObject.getTags().get(1));
+            if (offerObject.getTags().size() > 1) {
+                holder.tag2.setText(offerObject.getTags().get(1));
+            } else {
+                holder.tag2.setVisibility(View.GONE); // Si no hay etiqueta, oculta la vista
             }
 
-            if (offerObject.getTags().get(2) != null) {
-                holder.tag1.setText(offerObject.getTags().get(2));
+            if (offerObject.getTags().size() > 2) {
+                holder.tag3.setText(offerObject.getTags().get(2));
+            } else {
+                holder.tag3.setVisibility(View.GONE); // Si no hay etiqueta, oculta la vista
             }
 
         }
@@ -106,13 +112,13 @@ public class Offer extends Fragment {
         class OffersViewHolder extends RecyclerView.ViewHolder {
 
             //Faltan campos
-            TextView offerName, companyName, tag1, tag2, tag3;
+            TextView name, companyName, tag1, tag2, tag3;
 
             public OffersViewHolder(@NonNull View itemView) {
                 super(itemView);
 
-                offerName = itemView.findViewById(R.id.offerName);
-                companyName = itemView.findViewById(R.id.offerName);
+                name = itemView.findViewById(R.id.offerName);
+                companyName = itemView.findViewById(R.id.companyName);
                 tag1 = itemView.findViewById(R.id.btnTagOne);
                 tag2 = itemView.findViewById(R.id.btnTagTwo);
                 tag3 = itemView.findViewById(R.id.btnTagThree);
