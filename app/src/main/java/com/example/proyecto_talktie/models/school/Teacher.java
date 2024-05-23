9aca80f594b6a24274b400d5af24eb40644fb661
Bokurai <mrnere88@gmail.com>
2024-05-23 19:24:01 +0200

    Comentarios modelos

--- app/src/main/java/com/example/proyecto_talktie/models/school/Teacher.java
+++ app/src/main/java/com/example/proyecto_talktie/models/school/Teacher.java
@@ -2,18 +2,55 @@ package com.example.proyecto_talktie.models.school;
 
 import java.util.List;
 
+/*
+ * Class that represents the School sub-type named Teacher.
+ */
 public class Teacher {
+
+    /*
+     * Name of the teacher.
+     */
     String name;
+
+    /*
+     * Profile image of the teacher.
+     */
     String profileImage;
+
+    /*
+     * Id of the teacher.
+     */
     String teacherId;
+
+    /*
+     * Email of the teacher.
+     */
     String email;
+
+    /*
+     * Position of the teacher.
+     */
     String position;
+
+    /*
+     * Id of the teacher's school.
+     */
     String schoolId;
+
+    /*
+     * Students recommended by the teacher.
+     */
     List<String> recommendedStudents;
 
+    /*
+     * Empty constructor required by Firebase.
+     */
     public Teacher() {
     }
 
+    /*
+     * Constructor with some properties.
+     */
     public Teacher(String name, String schoolId) {
         this.name = name;
         this.schoolId = schoolId;
